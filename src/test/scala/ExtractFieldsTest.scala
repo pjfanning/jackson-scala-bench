c658f5ac86ae6bffa78e4ac603b76149eda609eb
PJ Fanning <pjfanning@users.noreply.github.com>
2022-12-13 15:14:56 +0100

    Update ExtractFieldsTest.scala

--- src/test/scala/ExtractFieldsTest.scala
+++ src/test/scala/ExtractFieldsTest.scala
@@ -8,9 +8,7 @@ class ExtractFieldsTest extends AnyFlatSpec with Matchers {
 
   "ExtractFields" should "handle unknown json field" in {
     val stringBuilder = new StringBuilder
-    (0 until 1000000).foreach {
-      stringBuilder.append(7)
-    }
+    (0 until 1000000).foreach(_ => stringBuilder.append(7))
     val test1000000 = stringBuilder.toString
     val jacksonMapper = createJacksonMapper()
     val result = jacksonMapper.readValue[ExtractFields](ExtractFields.genJson(test1000000))
