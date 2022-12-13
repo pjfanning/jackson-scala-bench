6a37ef1c0214cc2265a899acc2c3367fc2a033aa
PJ Fanning <pjfanning@users.noreply.github.com>
2022-12-13 15:05:17 +0100

    add benchmark

--- build.sbt
+++ build.sbt
@@ -2,6 +2,8 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.10"
 
+enablePlugins(JmhPlugin)
+
 lazy val root = (project in file("."))
   .settings(
     name := "jackson-scala-bench",
---
+++ src/main/scala/ExtractFieldsBenchmark.scala
@@ -0,0 +1,28 @@
+package com.github.pjfanning.jackson.scala
+
+import org.openjdk.jmh.annotations.Benchmark
+
+import java.nio.charset.StandardCharsets
+
+class ExtractFieldsBenchmark extends CommonParams {
+  val (val1000, val1000000) = {
+    val stringBuilder = new StringBuilder
+    (0 until 1000).foreach(_ => stringBuilder.append(7))
+    val v1000 = stringBuilder.toString()
+    (1000 until 1000000).foreach(_ => stringBuilder.append(7))
+    (v1000, stringBuilder.toString())
+  }
+  val test1000 = ExtractFields.genJson(val1000).getBytes(StandardCharsets.UTF_8)
+  val test1000000 = ExtractFields.genJson(val1000000).getBytes(StandardCharsets.UTF_8)
+  val mapper = JacksonUtil.createJacksonMapper()
+
+  @Benchmark
+  def deser1000 = {
+    mapper.readValue(test1000, classOf[ExtractFields])
+  }
+
+  @Benchmark
+  def deser1000000 = {
+    mapper.readValue(test1000000, classOf[ExtractFields])
+  }
+}
